f65571cdcf0d37b27799549562e9dbc66f22825b
Ahmedbht <ahmedbouhtit8@gmail.com>
2026-08-22 15:19:09 +0100

    add cdde

--- lib/providers/medication_provider.dart
+++ lib/providers/medication_provider.dart
@@ -4,16 +4,29 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uuid/uuid.dart';
 import '../models/medication.dart';
 
-class MedicationProvider extends ChangeNotifier{
+class MedicationProvider extends ChangeNotifier {
   final _uuid = const Uuid();
   List<Medication> _medications = [];
 
-  static const String _storageKey ='medications';
+  static const String _storageKey = 'medications';
 
-  Future<void> loadMedications() async{
+  Future<void> loadMedications() async {
     final prefs = await sharedPreferences.getInstance();
     final jsonString = prefs.getString(_storageKey);
 
-    
+    if (jsonString != null) {
+      final List<dynamic> jsonList = json.decode(jsonString);
+      _medications = jsonList.map((json) => Medication.fromJson(json)).toList();
+    }
+    notifyListeners();
   }
+
+  Future<void> saveMedications() async {
+    final prefs = await sharedPreferences.getInstance();
+    final jsonList = _medications.map((m) => m.toJson()).toList();
+    await prefs.setString(_storageKey, json.encode(jsonList));
+  }
+
+  //add a brand new medication
+  
 }
