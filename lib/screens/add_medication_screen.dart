f6e7b942cf476e1351e0561c65e7197ebcea7549
Ahmedbht <ahmedbouhtit8@gmail.com>
2026-08-23 20:37:29 +0100

    save

--- lib/screens/add_medication_screen.dart
+++ lib/screens/add_medication_screen.dart
@@ -23,6 +23,34 @@ class _AddMedicationScreenState extends State<AddMedicationScreen> {
     'Injection',
   ];
 
+  Future<void> _pickTime() async {
+    final picked = await showTimePicker(
+      context: context,
+      initialTime: _selectedTime,
+    );
+    if (picked != null) {
+      setState(() {
+        _selectedTime = picked;
+      });
+    }
+  }
 
-  
+  void _save(){
+    if(_nameController.text.trim()isEmpty){
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Please enter a medication name'),
+      ));
+      return;
+    }
+
+  final timeString = _selectedTime.format(context);
+
+  Provider.of<MedicationProvider>(context, listen: false).addListener( name :_nameController.text.trim(),
+  dosage: _dosageController.text.trim(),
+  form: _form,
+  time :timeString,
+  note: _noteController.text.trim(),);
+
+  Navigator.pop(context);
+  }
 }
