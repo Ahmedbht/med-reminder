38bc775dbd1bdba614a4c21c67872cd92ed6250c
Ahmedbht <ahmedbouhtit8@gmail.com>
2026-08-24 21:13:21 +0100

    nots

---
+++ lib/services/notification_service.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package :timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
+
+class NotificationService {
+  final FlutterLocalNotificationsPlugin _plugin =
+      FlutterLocalNotificationsPlugin();
+  Future<void> init() async {
+    tz.initializeTimeZones();
+    const androidSettings = AndroidInitializationSettings(
+      '@mipmap/ic_launcher',
+    );
+    const iosSettings = DarwinInitializationSettings();
+    const settings = InitializationSettings(
+      android: androidSettings,
+      iOS: iosSettings,
+    );
+
+    Future<void> scheduleNotification({
+      required int id,
+      required String title,
+      required String body,
+      required int hour,
+      required int minute,
+    }) async {
+      final now = tz.TZDateTime.now(tz.local);
+      var scheduledDate = (
+        tz.local,
+        now.year,
+        now.month,
+        now.day,
+        hour,
+        minute,
+      );
+      
+
+      if(scheduledDate.isBefore(now)){
+        scheduledDate = scheduledDate.add(const Duration(days: 1));
+      }
+
+
+      
+
+      await _plugin.zonedSchedule(id,
+      title,
+      body,
+      scheduledDate,
+      const NotificationDetails(
+        android: AndroidNotificationDetails('med_channel', 'Medication Reminders'),
+        iOS: DarwinNotificationDetails(),
+)
+    }
+  }
+}id
