907b9b5729b51f4b898dd0fc0551193ee2873a90
Ahmedbht <ahmedbouhtit8@gmail.com>
2026-08-24 21:16:14 +0100

    fix file errs

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,47 +1,40 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package :timezone/timezone.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
 
 class NotificationService {
-  final FlutterLocalNotificationsPlugin _plugin =
-      FlutterLocalNotificationsPlugin();
+  final FlutterLocalNotificationsPlugin _plugin = FlutterLocalNotificationsPlugin();
+
   Future<void> init() async {
     tz.initializeTimeZones();
-    const androidSettings = AndroidInitializationSettings(
-      '@mipmap/ic_launcher',
-    );
+
+    const androidSettings = AndroidInitializationSettings('@mipmap/ic_launcher');
     const iosSettings = DarwinInitializationSettings();
+
     const settings = InitializationSettings(
       android: androidSettings,
       iOS: iosSettings,
     );
 
-    Future<void> scheduleNotification({
-      required int id,
-      required String title,
-      required String body,
-      required int hour,
-      required int minute,
-    }) async {
-      final now = tz.TZDateTime.now(tz.local);
-      var scheduledDate = (
-        tz.local,
-        now.year,
-        now.month,
-        now.day,
-        hour,
-        minute,
-      );
-      
+    await _plugin.initialize(settings);
+  }
 
-      if(scheduledDate.isBefore(now)){
-        scheduledDate = scheduledDate.add(const Duration(days: 1));
-      }
+  Future<void> scheduleNotification({
+    required int id,
+    required String title,
+    required String body,
+    required int hour,
+    required int minute,
+  }) async {
+    final now = tz.TZDateTime.now(tz.local);
+    var scheduledDate = tz.TZDateTime(tz.local, now.year, now.month, now.day, hour, minute);
 
+    if (scheduledDate.isBefore(now)) {
+      scheduledDate = scheduledDate.add(const Duration(days: 1));
+    }
 
-      
-
-      await _plugin.zonedSchedule(id,
+    await _plugin.zonedSchedule(
+      id,
       title,
       body,
       scheduledDate,
@@ -51,7 +44,6 @@ class NotificationService {
       ),
       androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
       matchDateTimeComponents: DateTimeComponents.time,
-      );
-    }
+    );
   }
 }
