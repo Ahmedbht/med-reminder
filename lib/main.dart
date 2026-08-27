544c4f3fbd785b7c689fe44687a4382760d231eb
Ahmedbht <ahmedbouhtit8@gmail.com>
2026-08-27 18:27:34 +0100

    tyest

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/medication_provider.dart';
 import 'screens/home_screen.dart';
+import 'services/notification_service.dart';
 
 void main() {
   runApp(
