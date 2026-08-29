171b2e63ceaee66a65f2c775de2967b3092fd0ad
Ahmedbht <ahmedbouhtit8@gmail.com>
2026-08-29 22:14:37 +0100

    history

---
+++ lib/screens/history_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:table_calendar/table_calendar.dart';
+import '../providers/medication_provider.dart';
+
+class HistoryScreen extends StatefulWidget {
+  const HistoryScreen({super.key});
+
+  @override
+  State<HistoryScreen> createState() => _HistoryScreenState();
+}
+
+
