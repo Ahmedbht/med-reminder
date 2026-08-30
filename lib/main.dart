612c5674759015cf5a3ba3e1131ee1cbc1d83c09
Ahmedbht <ahmedbouhtit8@gmail.com>
2026-08-30 22:22:49 +0100

    fix err 2

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class RootScreen extends StatefulWidget {
 }
 
 class _RootScreenState extends State<RootScreen> {
-  int _currenIndex = 0;
+  int _currentIndex = 0;
 
   final List<Widget> _screens = const [Homescreen(), HistoryScreen()];
 
@@ -44,10 +44,10 @@ class _RootScreenState extends State<RootScreen> {
     return Scaffold(
       body: _screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currenIndex,
+        currentIndex: _currentIndex,
         onTap: (index) {
           setState(() {
-            _currenIndex = index;
+            _currentIndex = index;
           });
         },
         items: const [
